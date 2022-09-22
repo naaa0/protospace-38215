dc07b7aab559a54cd778c5f4ae61406ab7223e60
naaa0 <lil.nuts70@gmail.com>
2022-09-22 15:50:23 +0900

    空のコメント投稿時のエラー解消

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,9 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype.id)
     else
-      render "/protorypes/#{comment.prototype.id}"
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
 
